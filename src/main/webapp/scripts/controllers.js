e1b790ef9ce298b408d04fbf93f50a460bcf0407
Marco Bolognesi <Marco.Bolognesi@external.bnlmail.com>
2016-02-03 17:56:00 +0100

    prime prove (non funzionanti) di routing con angular

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -1,6 +1,30 @@
-var app=angular.module('beer',[]).controller('recipes', function recipes($scope, $http) {
-    $http.get('http://localhost:8080/beerdiaries/api/recipe/all').
-        success(function(data) {
-            $scope.recipes = data;
-        });
-});
+var app = angular.module('beer', [ 'recipesControllers', 'ngRoute' ]);
+
+var recipesControllers = angular.module('recipesControllers', []);
+
+recipesControllers.controller('recL', [
+		'$scope',
+		'$http',
+		function recipes($scope, $http) {
+			$http.get('http://localhost:8080/beerdiaries/api/recipe/all')
+					.success(function(data) {
+						$scope.recipes = data;
+					});
+		} ]);
+
+recipesControllers.controller('recI', [ '$scope', '$routeParams',
+		function recipes($scope, $routeParams) {
+			$scope.recipe = $routeParams.recipe;
+		} ]);
+
+app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/recipes', {
+		templateUrl : 'views/recL.html',
+		controller : 'recL'
+	}).when('/recipes/:recipe', {
+		templateUrl : 'views/recI.html',
+		controller : 'recI'
+	}).otherwise({
+		redirectTo : '/recipes'
+	});
+} ]);

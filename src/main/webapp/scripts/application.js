74623490744e725db3afecd6740dc8a691aabd0e
Marco Bolognesi <Marco.Bolognesi@external.bnlmail.com>
2016-02-08 13:43:41 +0100

    comincia a funzionare...intanto con rootScope

--- src/main/webapp/scripts/application.js
+++ src/main/webapp/scripts/application.js
@@ -6,9 +6,34 @@ bdctrls.controller(
 		'loginController',
 		[
 		 '$scope',
-		 'logged',
-		 function loginController($scope, loggedin){
-			 $scope.logged=loggedin;
+		 '$rootScope',
+		 function loginController($scope, $rootScope){
+			 $rootScope.logged=true;
+		 }
+		]
+);
+
+bdctrls.controller(
+		'recipesController',
+		[
+		 '$scope',
+         '$http',
+         function recipes($scope, $http) {
+			 $http.get('http://localhost:8080/beerdiaries/api/recipe/all')
+             	.success(function(data) {
+             		$scope.recipes = data;
+                }
+             );
+		 } 
+		]
+);
+bdctrls.controller(
+		'logoutController',
+		[
+		 '$scope',
+		 '$rootScope',
+		 function logoutController($scope, $rootScope){
+			 $rootScope.logged=false;
 		 }
 		]
 );
@@ -18,6 +43,21 @@ bdapp.config(
 		 '$routeProvider', 
 		 function($routeProvider) {
 			$routeProvider
+			.when('/reg', {
+				templateUrl : 'views/registration.html',
+				controller : 'loginController',
+				resolve: true
+			})
+			.when('/recipes', {
+				templateUrl : 'views/recipes.html',
+				controller : 'recipesController',
+				resolve: true
+			})
+			.when('/diary', {
+				templateUrl : 'views/diary.html',
+				controller : 'loginController',
+				resolve: true
+			})
 			.when('/login', {
 					templateUrl : 'views/loggedin.html',
 					controller : 'loginController',
@@ -25,12 +65,12 @@ bdapp.config(
 				})
 			.when('/logout', {
 					templateUrl : 'views/guest.html',
-					controller : 'loginController',
+					controller : 'logoutController',
 					resolve: false
 				})
 			.otherwise({
-					redirectTo : 'guest.html',
-					controller : 'homeController'
+					redirectTo : 'views/guest.html',
+					controller : 'loginController'
 				});
 		 }
 		]

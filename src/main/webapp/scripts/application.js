1de8b0926d313e5d691c0b2bf95584aa3baec34d
miogituser <bolognesi.marco@gmail.com>
2016-02-14 23:26:37 +0100

    authentication authorization...etc

--- src/main/webapp/scripts/application.js
+++ src/main/webapp/scripts/application.js
@@ -2,9 +2,109 @@ var bdapp = angular.module('beerdiariesApplication', [ 'ngRoute', 'bdctrls' ]);
 
 var bdctrls = angular.module('bdctrls', []);
 
+bdapp.constant('AUTH_EVENTS', {
+	loginSuccess : 'auth-login-success',
+	loginFailed : 'auth-login-failed',
+	logoutSuccess : 'auth-logout-success',
+	sessionTimeout : 'auth-session-timeout',
+	notAuthenticated : 'auth-not-authenticated',
+	notAuthorized : 'auth-not-authorized'
+});
+
+bdapp.constant('USER_ROLES', {
+	all : '*',
+	admin : 'admin',
+	editor : 'editor',
+	guest : 'guest'
+});
+
+bdctrls.controller('ApplicationController', [ '$scope', 'USER_ROLES',
+		'AuthService',
+
+		function($scope, USER_ROLES, AuthService) {
+			$scope.currentUser = null;
+			$scope.userRoles = USER_ROLES;
+			$scope.isAuthorized = AuthService.isAuthorized;
+
+			$scope.setCurrentUser = function(user) {
+				$scope.currentUser = user;
+			};
+		} ]);
+
+bdctrls.controller('LoginController', [ '$scope', '$rootScope', 'AUTH_EVENTS',
+		'AuthService', function($scope, $rootScope, AUTH_EVENTS, AuthService) {
+			$scope.credentials = {
+				username : '',
+				password : ''
+			};
+			$scope.login = function(credentials) {
+				AuthService.login(credentials).then(function(user) {
+					$rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+					$scope.setCurrentUser(user);
+				}, function() {
+					$rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+				});
+			};
+		} ]);
+
+bdapp.factory('AuthService', function($http, Session) {
+	var authService = {};
+
+	authService.login = function(credentials) {
+		return $http.post('/login', credentials).then(function(res) {
+			Session.create(res.data.id, res.data.user.id, res.data.user.role);
+			return res.data.user;
+		});
+	};
+
+	authService.isAuthenticated = function() {
+		return !!Session.userId;
+	};
+
+	authService.isAuthorized = function(authorizedRoles) {
+		if (!angular.isArray(authorizedRoles)) {
+			authorizedRoles = [ authorizedRoles ];
+		}
+		return (authService.isAuthenticated() && authorizedRoles
+				.indexOf(Session.userRole) !== -1);
+	};
+
+	return authService;
+});
+
+bdapp.service('Session', function() {
+	this.create = function(sessionId, userId, userRole) {
+		this.id = sessionId;
+		this.userId = userId;
+		this.userRole = userRole;
+	};
+	this.destroy = function() {
+		this.id = null;
+		this.userId = null;
+		this.userRole = null;
+	};
+});
+
+bdapp.run(function($rootScope, AUTH_EVENTS, AuthService) {
+	$rootScope.$on('$routeChangeStart ', function(event, next) {
+		var authorizedRoles = next.data.authorizedRoles;
+		if (!AuthService.isAuthorized(authorizedRoles)) {
+			event.preventDefault();
+			if (AuthService.isAuthenticated()) {
+				// user is not allowed
+				$rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+			} else {
+				// user is not logged in
+				$rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+			}
+		}
+	});
+});
+
 bdctrls.controller('diaryController', [ '$scope', '$rootScope',
 		function diaryController($scope, $rootScope) {
 		} ]);
+
 bdctrls.controller('registrationController', [
 		'$scope',
 		'$rootScope',
@@ -17,15 +117,15 @@ bdctrls.controller('registrationController', [
 						'http://localhost:8080/beerdiaries/api/recipe/regUser',
 						$scope.user);
 				res.success(function(data, status, headers, config) {
-						$location.path("/loggedin");
-						$scope.$apply();
-					}
-				);
-				
+					$location.path("/																									loggedin");
+					$scope.$apply();
+				});
+
 				res.error(function(data, status, headers, config) {
-						alert(JSON.stringify({data: data}));
-					}
-				);
+					alert(JSON.stringify({
+						data : data
+					}));
+				});
 				alert($scope.user.username);
 			};
 
@@ -43,9 +143,7 @@ bdctrls.controller('loggedinController', [ '$scope', '$rootScope',
 		function loggedinController($scope, $rootScope) {
 			$rootScope.logged = true;
 		} ]);
-bdctrls.controller('loginController', [ '$scope', '$rootScope',
-		function loginController($scope, $rootScope) {
-		} ]);
+
 bdctrls.controller('recipeController', [
 		'$scope',
 		'$http',
@@ -71,31 +169,55 @@ bdctrls.controller('logoutController', [ '$scope', '$rootScope',
 			$rootScope.logged = false;
 		} ]);
 
-bdapp.config([ '$routeProvider', function($routeProvider) {
+bdapp.config([ '$routeProvider','USER_ROLES', function($routeProvider, USER_ROLES) {
 	$routeProvider.when('/reg', {
 		templateUrl : 'views/registration.html',
-		controller : 'registrationController'
+		controller : 'registrationController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all, USER_ROLES.all ]
+		}
 	}).when('/recipes', {
 		templateUrl : 'views/recipes.html',
-		controller : 'recipesController'
+		controller : 'recipesController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).when('/diary', {
 		templateUrl : 'views/diary.html',
-		controller : 'diaryController'
+		controller : 'diaryController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).when('/login', {
 		templateUrl : 'views/login.html',
-		controller : 'loginController'
+		controller : 'LoginController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).when('/loggedin', {
 		templateUrl : 'views/loggedin.html',
-		controller : 'loggedinController'
+		controller : 'loggedinController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).when('/logout', {
 		templateUrl : 'views/guest.html',
-		controller : 'logoutController'
+		controller : 'logoutController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).when('/recipe/:recipeid', {
 		templateUrl : 'views/recipeDetail.html',
-		controller : 'recipeController'
+		controller : 'recipeController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	}).otherwise({
 		redirectTo : 'views/guest.html',
-		controller : 'logoutController'
+		controller : 'logoutController',
+		data : {
+			authorizedRoles : [ USER_ROLES.all ]
+		}
 	});
 } ]);
 
